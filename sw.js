2ba6012c802b3120699606552767e9ad3b253d4b
codeAlphaBravosite <help.factchannel@gmail.com>
2024-12-13 00:38:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = '1.1.0';
-const CACHE_NAME = `storyboard-cache-${VERSION}`;
+const VERSION = '1.0.1';
+const CACHE_NAME = `osm-cache-${VERSION}`;
 
 const STATIC_CACHE_URLS = [
   './',
@@ -22,7 +22,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName.startsWith('storyboard-cache-') && cacheName !== CACHE_NAME) {
+          if (cacheName.startsWith('osm-cache-') && cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
